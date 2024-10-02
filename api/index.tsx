9b3e0b570cc8b1c7027a563bf7568befd954729f
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-10-01 22:29:01 -0500

    dvgsddbswvb

--- api/index.tsx
+++ api/index.tsx
@@ -234,7 +234,7 @@ app.frame('/game', async (c) => {
         justifyContent: 'center',
         width: '1080px',
         height: '1080px',
-        backgroundImage: 'url(https://bafybeif6knruive34r5sebbr5jcu6w65sast2hivnouqeid3dolyn76e3u.ipfs.w3s.link/Frame%2025%20(4).png)',
+        backgroundImage: 'url(https://bafybeigp3dkqr7wqgvp7wmycpg6axhgmc42pljkzmhdbnrsnxehoieqeri.ipfs.w3s.link/Frame%209.png)',
         backgroundSize: '1080px 1080px', // Fixed size matching the container
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat', // Prevent repeating
