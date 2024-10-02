a8b98128857ac37ea0a0a2e74db90bdf65ef4a50
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-10-02 11:11:58 -0500

    finalfornow

--- api/index.tsx
+++ api/index.tsx
@@ -96,7 +96,7 @@ app.frame('/', () => {
       <meta property="fc:frame" content="vNext">
       <meta property="fc:frame:image" content="${gifUrl}">
       <meta property="fc:frame:image:aspect_ratio" content="1:1">
-      <meta property="fc:frame:button:1" content="Press Start">
+      <meta property="fc:frame:button:1" content="Start">
       <meta property="fc:frame:button:1:action" content="post">
       <meta property="fc:frame:post_url" content="${baseUrl}/api/howtoplay">
       
